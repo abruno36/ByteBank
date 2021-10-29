d988ac8e833aae2669f1d64b4e1c2dd66be17978
= <=>
2021-10-29 15:46:35 -0300

    alterações main - 29101546

--- lib/main.dart
+++ lib/main.dart
@@ -3,33 +3,33 @@ import 'package:flutter/material.dart';
 const primaryColor = Color(0xFF1B5E20);
 const scaffoldColor = Color(0xFFE8F5E9);
 
-void main() => runApp(MaterialApp(
+void main() => runApp(BytebankApp());
+
+
+class BytebankApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: scaffoldColor,
-        body: ListaTransferencias(),
-        appBar: AppBar(
-          backgroundColor: primaryColor,
-          title: Text('Transferências'),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          backgroundColor: Colors.green,
-          onPressed: () {},
-        ),
-        bottomNavigationBar: BottomAppBar(
-          shape: CircularNotchedRectangle(),
-          color: primaryColor,
-          child: Container(height: 50)
-        ),
+        body: FormularioTransferencia(),
       ),
-    ));
+    );
+  }
+}
 
 class Transferencia {
   final double valor;
   final int numeroConta;
 
   Transferencia(this.valor, this.numeroConta);
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, numeroConta: $numeroConta}';
+  }
 }
 
 class ItemTransferencia extends StatelessWidget {
@@ -55,13 +55,92 @@ class ListaTransferencias extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Column(
-        children: <Widget>[
-          ItemTransferencia(Transferencia(100.0, 1000)),
-          ItemTransferencia(Transferencia(200.0, 2000)),
-          ItemTransferencia(Transferencia(300.0, 4000)),
-          ItemTransferencia(Transferencia(5500.80, 4000)),
-        ]
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        title: Text('Transferências'),
+      ),
+      body: Column(
+          children: <Widget>[
+            ItemTransferencia(Transferencia(100.0, 1000)),
+            ItemTransferencia(Transferencia(200.0, 2000)),
+            ItemTransferencia(Transferencia(300.0, 4000)),
+            ItemTransferencia(Transferencia(5500.80, 4000)),
+          ]
+      ),
+
+
     );
   }
 }
+
+class FormularioTransferencia extends StatelessWidget {
+
+  final TextEditingController _controladorCampoNumeroConta = TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        title: Text('Criando Transferência'),),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controladorCampoNumeroConta,
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+              decoration: InputDecoration(
+                  labelText: 'Número da conta',
+                  hintText: '0000'
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controladorCampoValor,
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+              decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on, color: Colors.green,),
+                  labelText: 'Valor',
+                  hintText: '0.00'
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          RaisedButton(
+            child: Text('Confirmar'),
+            onPressed: () {
+              //debugPrint('clicou no confirmar');
+              //debugPrint(_controladorCampoNumeroConta.text);
+              //debugPrint(_controladorCampoValor.text);
+              final int? numeroConta =
+              int.tryParse(_controladorCampoNumeroConta.text);
+              final double? valor =
+              double.tryParse(_controladorCampoValor.text);
+              if (numeroConta != null && valor != null) {
+                final transferenciaCriada = Transferencia(valor, numeroConta);
+                debugPrint('$transferenciaCriada');
+              }
+            }
+             ),
+          ],
+        ),
+      bottomNavigationBar: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          color: primaryColor,
+          child: Container(height: 50)
+      ),
+  );
+  }
+}
+
+
